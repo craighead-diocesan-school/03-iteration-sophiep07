2c336c09df2f42fb9857d4bcd85f6445f27c896f
sophiep07 <parsonso14@craighead.school.nz>
2023-06-12 11:47:39 +1200

    did square series

--- code/3-square-series.js
+++ code/3-square-series.js
@@ -3,5 +3,16 @@
 // ####################################
 
 function squareSeries() {
-  // write your code here
+  let number = prompt('What number do you want the square series for?')
+  let count = 1
+  let series = 'Series for ' + number + ' is: '
+
+  while (count <= number) {
+    series = series + (count * count)
+    series = series + ', '
+    count = count + 1
+  }
+
+  alert(series)
+  
 }
