650ee5e213cf29a47c1386a3624f525669c46b15
sophiep07 <parsonso14@craighead.school.nz>
2023-06-13 10:16:55 +1200

    did fibonacci

--- code/5-ext-fibonacci.js
+++ code/5-ext-fibonacci.js
@@ -3,5 +3,23 @@
 // ####################################
 
 function fibonacci() {
-  // write your code here
+  let max = prompt('What number do you want the Fibonacci Sequence up to?')
+  let count = 0
+  count = Number(count)
+  let sequence = 'The Fibonacci Sequence is: '
+  let old = 0
+  let prev = 1
+  let next = ' '
+
+  while (count < max) {
+    next = old + prev
+    old = prev
+    prev = next
+    sequence = sequence + ' ' + next
+    count = next
+  }
+
+
+  alert(sequence)
+
 }
