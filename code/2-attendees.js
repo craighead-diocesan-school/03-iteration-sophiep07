b96fe8236c05560ff4ccd4a042b7c09f148a0913
sophiep07 <parsonso14@craighead.school.nz>
2023-07-18 10:42:02 +1200

    fixed

--- code/2-attendees.js
+++ code/2-attendees.js
@@ -3,31 +3,28 @@
 // ####################################
 
 function attendees() {
-
   let count = 0
-  let numOfAttendees = prompt('How many attendees are there?')
-  let attendeeList = ''
-  
+  let numOfAttendees = prompt("How many attendees are there?")
+  let attendeeList = ""
+
   while (count < numOfAttendees) {
-    let attendee = prompt('What is the name of the attendee?')
-    attendeeList = attendeeList + attendee + ', ' 
+    let attendee = prompt("What is the name of the attendee?")
+    attendeeList = attendeeList + attendee + ", "
     count = count + 1
-  
-  alert(attendeeList) 
+
+    alert(attendeeList)
+  }
 }
 
 function make2() {
-
-  let numOfNumbers = prompt('How many numbers do you want to add?')
+  let numOfNumbers = prompt("How many numbers do you want to add?")
   let count = 0
   let total = 0
-
   while (count < numOfNumbers) {
-    let numbers = prompt('What are one of the numbers?')
+    let numbers = prompt("What are one of the numbers?")
     numbers = Number(numbers)
     total = total + numbers
     count = count + 1
   }
-  
-  alert('The sum of those numbers is: ' + total)
+  alert("The sum of those numbers is: " + total)
 }
